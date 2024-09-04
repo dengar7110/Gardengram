025e7b1aff6990fdd3188626579e9a09c5614f3e
garden <lmnoptnt@gmail.com>
2024-09-04 21:51:56 +0900

    [홍정원] - Gardengram 프로젝트 최초 생성

---
+++ src/main/java/com/garden/gardengram/GardengramApplication.java
@@ -0,0 +1,13 @@
+package com.garden.gardengram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardengramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardengramApplication.class, args);
+	}
+
+}
